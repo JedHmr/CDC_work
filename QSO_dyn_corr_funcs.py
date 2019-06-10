3fa1372f64ceed230d81e21fd0137d2948427848
Jed Homer <jedhmr@gmail.com>
2019-06-10 13:47:56 +0200

    3D plot, back indexing sorted

--- QSO_dyn_corr_funcs.py
+++ QSO_dyn_corr_funcs.py
@@ -3,6 +3,8 @@
 import os
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib import cm, colors
+from mpl_toolkits.mplot3d import Axes3D
 
 from astropy import units as u
 from astropy.coordinates import SkyCoord
@@ -76,30 +78,15 @@ def radius_match(coords,coords_errs,cat_mask):
         coords_matches_err.append(coords_errs[idxcatalog])
         cat_idx_matches.append(catalog_idxmsk)
         
-        print('mask ',sum(catalogmsk))
-        print('idx_cat',sum(idxcatalog))
-        #print(coords[idxcatalog])
-        print(len(coords_matches)) 
+        print('mask: ',sum(catalogmsk))
+        print('idx_cat:',sum(idxcatalog))
+        print('coord: ',coords[idxcatalog])
+        print('count: ',len(coords_matches)) 
     
-    return np.concatenate(coords_matches), np.concatenate(coords_matches_err), cat_idx_matches
+    return np.concatenate(coords_matches), np.concatenate(coords_matches_err), np.concatenate(cat_idx_matches)
 
-def error_filter_count(source_errs, bound):
-    
-    """
-        number of sources with error < bound
-    """
-    count = 0
-    other = 0
-    for obj in source_errs:
-        if obj < 1.5:
-            count += 1
-        else:
-            other += 1
-            
-    return count
 
 # inital matches with 2" radius
-
 DR7_coords_init, DR7_coords_errs_init, XMMa_coords_init, XMMa_coords_errs_init, cat_maskDR7 = initial_match(coords_DR7, coords_3XMM, dpos1, 2.0)
 DR12_coords_init, DR12_coords_errs_init, XMMb_coords_init, XMMb_coords_errs_init, cat_maskDR12 = initial_match(coords_DR12, coords_3XMM, dpos1, 2.0)
 
@@ -107,67 +94,62 @@ DR12_coords_init, DR12_coords_errs_init, XMMb_coords_init, XMMb_coords_errs_init
 DR7_matches, DR7_matches_poserr, XMM_DR7_idx_matches = radius_match(DR7_coords_init, DR7_coords_errs_init, cat_maskDR7)
 DR12_matches, DR12_matches_poserr, XMM_DR12_idx_matches = radius_match(DR12_coords_init, DR12_coords_errs_init, cat_maskDR12)
 # matched coordinates SDSS->3XMM->3XMM (dyn. errs.) (ARE THESE NEEDED NOW ONE HAS XMM_DR#_idx_matches)
-XMMa_matches, XMMa_matches_poserr = radius_match(XMMa_coords_init, XMMa_coords_errs_init)
-XMMb_matches, XMMb_matches_poserr = radius_match(XMMb_coords_init, XMMb_coords_errs_init)
+#XMMa_matches, XMMa_matches_poserr = radius_match(XMMa_coords_init, XMMa_coords_errs_init)
+#XMMb_matches, XMMb_matches_poserr = radius_match(XMMb_coords_init, XMMb_coords_errs_init)
 
-count_DRcats = error_filter_count(DR7_matches_poserr,1.5) + error_filter_count(DR12_matches_poserr,1.5)
+# %% matches to tables back matching
 
+# any point in having coordinates????? just use indexes out of functions ?
+
+table_3XMM_DR7_matches = t_3XMM[XMM_DR7_idx_matches[(DR7_matches_poserr < 1.5)]]
+table_3XMM_DR12_matches = t_3XMM[XMM_DR12_idx_matches[(DR12_matches_poserr < 1.5)]]
+
+print(len(table_3XMM_DR7_matches) + len(table_3XMM_DR12_matches))
+
+csDR7 = SkyCoord(table_3XMM_DR7_matches['SC_RA'],table_3XMM_DR7_matches['SC_DEC'])
+csDR12 =  SkyCoord(table_3XMM_DR12_matches['SC_RA'],table_3XMM_DR12_matches['SC_DEC'])
 
 # %% spherical plots
-#
-## Create a sphere
-#r, pi = 1, np.pi
-#phi, theta = np.mgrid[0.0:pi:100j, 0.0:2.0*pi:100j]
-#x = r*np.sin(phi)*np.cos(theta)
-#y = r*np.sin(phi)*np.sin(theta)
-#z = r*np.cos(phi)
-#
-## func for spherical coords of catalogs
-#def coords_to_sphericals(coords):
-#    theta, phi = coords.ra.rad, coords.dec.rad
-#    xx = np.sin(phi)*np.cos(theta)
-#    yy = np.sin(phi)*np.sin(theta)
-#    zz = np.cos(phi)
-#
-#    return xx,yy,zz
-#
-#def coords_to_sphericals1(coords):
-#    theta, phi = coords, coords
-#    xx = np.sin(phi)*np.cos(theta)
-#    yy = np.sin(phi)*np.sin(theta)
-#    zz = np.cos(phi)
-#
-#    return xx,yy,zz
-#
-#
-#x1,y1,z1 = coords_to_sphericals(coords_3XMM)
-#x1a,y1a,z1a = coords_to_sphericals1(XMMa_matches)
-#x1c,y1c,z1c = coords_to_sphericals1(XMMb_matches)
-#x2,y2,z2 = coords_to_sphericals(coords_DR7)
-#x2a,y2a,z2a = coords_to_sphericals1(DR7_matches)
-#x3,y3,z3 = coords_to_sphericals(coords_DR12)
-#x3a,y3a,z3a = coords_to_sphericals1(DR12_matches)
-#
-#fig, axs = plt.subplots(3)
-#axs[0].plot_surface(x, y, z,  rstride=1, cstride=1, color='dimgray', alpha=0.15, linewidth=0)
-#axs[0].scatter(x1,y1,z1,color="plum",s=0.001)
-#axs[0].scatter(x1a,y1a,z1a,color="lightsalmon",s=0.001)
-#axs[0].scatter(x1a,y1a,z1a,color="lightsalmon",s=0.001)
-#axs[0].set_title('')
-#axs[1].plot_surface(x, y, z,  rstride=1, cstride=1, color='dimgray', alpha=0.15, linewidth=0)
-#axs[1].scatter(x2,y2,z2,color="plum",s=0.05)
-#axs[1].scatter(x2a,y2a,z2a,color="lightsalmon",s=0.05)
-#axs[1].set_title('')
-#axs[2].plot_surface(x, y, z,  rstride=1, cstride=1, color='dimgray', alpha=0.15, linewidth=0)
-#axs[2].scatter(x3,y3,z3,color="plum",s=0.05)
-#axs[2].scatter(x3a,y3a,z3a,color="lightsteelblue",s=0.05)
-#axs[2].set_title('')
-#
-#
-#axs.axis('off')
-#axs.set_xlim([-1,1])    
-#axs.set_ylim([-1,1])
-#axs.set_zlim([-1,1])
-#axs.set_facecolor('darkgrey') 
 
+# Create a sphere
+r, pi = 1, np.pi
+phi, theta = np.mgrid[0.0:pi:100j, 0.0:2.0*pi:100j]
+x = r*np.sin(phi)*np.cos(theta)
+y = r*np.sin(phi)*np.sin(theta)
+z = r*np.cos(phi)
+
+# func for spherical coords of catalogs
+def coords_to_sphericals(coords):
+    theta, phi = coords.ra.rad, coords.dec.rad
+    xx = np.sin(phi)*np.cos(theta)
+    yy = np.sin(phi)*np.sin(theta)
+    zz = np.cos(phi)
+
+    return xx, yy, zz
+
+# catalogs
+x1,y1,z1 = coords_to_sphericals(coords_3XMM)
+# matches 
+x1a,y1a,z1a = coords_to_sphericals(csDR7)
+x1b,y1b,z1b = coords_to_sphericals(csDR7)
+
+fig, ax = plt.subplots(1)
+ax = fig.add_subplot(111, projection='3d')
+
+ax.plot_surface(x, y, z,  rstride=1, cstride=1, color='w', alpha=0.3, linewidth=0.1)
+ax.scatter(x1,y1,z1,color="slategrey",s=0.001)
+ax.scatter(x1a,y1a,z1a,color="r",s=0.001)
+ax.scatter(x1b,y1b,z1b,color="b",s=0.001)
+
+# ax.scatter(coords_to_sphericals(coords_3XMM),color="slategrey",s=0.001)
+# ax.scatter(coords_to_sphericals(csDR7),color="r",s=0.001)
+# ax.scatter(coords_to_sphericals(csDR12),color="b",s=0.001)
+
+ax.axis('off')
+ax.set_aspect('equal')
+ax.set_xlim([-1,1])    
+ax.set_ylim([-1,1])
+ax.set_zlim([-1,1])
+ax.set_facecolor('darkgrey')
+ 
 #%%
