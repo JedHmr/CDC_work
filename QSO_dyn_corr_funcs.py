b33afc5309b049136fd2bf6e9e47246bb223c6b5
Jed Homer <jedhmr@gmail.com>
2019-06-21 12:40:45 +0200

    F_UV,F_X plot for SDSS DR7,DR12 QSOs

--- QSO_dyn_corr_funcs.py
+++ QSO_dyn_corr_funcs.py
@@ -21,6 +21,7 @@ from astropy import units as u
 from astropy.coordinates import SkyCoord
 from astropy.table import Table
 from astropy.coordinates import match_coordinates_sky
+from astropy.cosmology import FlatLambdaCDM
 
 from astroquery.irsa_dust import IrsaDust
 
@@ -129,6 +130,8 @@ DR12_matches, DR12_matches_poserr, XMM_DR12_idx_matches = radius_match(DR12_coor
 idx_bk_DR7, d2d_bk_DR7, d3d_bk_DR7 = DR7_matches.match_to_catalog_sky(coords_DR7)
 idx_bk_DR12, d2d_bk_DR12, d3d_bk_DR12 = DR12_matches.match_to_catalog_sky(coords_DR12)
 
+mask1, mask2 = d2d_bk_DR7 < 0.01*u.arcsec, d2d_bk_DR12 < 0.01*u.arcsec
+
 # %% matches to tables + back matching
 
 # any point in having coordinates????? just use indexes out of functions ?
@@ -138,12 +141,14 @@ t_3XMM_DR7_matches = t_3XMM[XMM_DR7_idx_matches[(DR7_matches_poserr < 1.5)]]
 t_3XMM_DR12_matches = t_3XMM[XMM_DR12_idx_matches[(DR12_matches_poserr < 1.5)]]
 
 # DR7, DR12 back matching IMPLEMENT THIS IN FUNCTIONS
-t_DR7_DR7_matches = t_DR7[idx_bk_DR7]
-t_DR12_DR12_matches = t_DR12[idx_bk_DR12]
+t_DR7_DR7_matches = t_DR7[idx_bk_DR7[(DR7_matches_poserr < 1.5)]]
+t_DR12_DR12_matches = t_DR12[idx_bk_DR12[(DR12_matches_poserr < 1.5)]]
 
-print('Quasars: ', len(t_3XMM_DR7_matches) + len(t_3XMM_DR12_matches))
-
+print('Quasars 3XMM->SDSS: ', len(t_3XMM_DR7_matches) + len(t_3XMM_DR12_matches))
+print('Quasars2 SDSS->SDSS: ', len(t_DR7_DR7_matches) + len(t_DR12_DR12_matches))
 # %% XRAY FLUX 
+
+# FLUXES ARE IN ERGS/CM^2
 
 #DR7,DR12 objs with 3XMM xray measures
 FX_soft_DR7 = t_3XMM_DR7_matches['SC_EP_2_FLUX'] + t_3XMM_DR7_matches['SC_EP_3_FLUX']
@@ -165,6 +170,10 @@ R_v, m = 3.1, sfdmap.SFDMap('C:/Users/jedhm/Documents/Projects/CDC_work/data/sfd
 # redshifts 'z' 
 z_DR7, z_DR12 = t_DR7_DR7_matches['REDSHIFT'], t_DR12_DR12_matches['Z_CIV']
 
+# remove bad redshifts
+z_DR12[z_DR12 < 0] = np.nan
+z_DR12 = np.ma.array(z_DR12, mask=np.isnan(z_DR12))
+
 # coord targets (refreshed)
 coords_DR7 = SkyCoord(t_DR7_DR7_matches['RA']*u.deg, t_DR7_DR7_matches['DEC']*u.deg)
 coords_DR12 = SkyCoord(t_DR12_DR12_matches['RA'], t_DR12_DR12_matches['DEC'])
@@ -175,9 +184,22 @@ ext_DR12 = R_v*m.ebv(coords_DR12) # R_v*E(B-V) = A_v
 
 # %% DR7 UV
 
-UV_DR7 = t_DR7_DR7_matches['LOGFNU2500A_ERGS_OBS']/z_DR7
+F_UV_DR7 = t_DR7_DR7_matches['LOGFNU2500A_ERGS_OBS'] # in log10(erg/cm^2/s)
 
-# %% DR12 UV FLUX
+cosmo = FlatLambdaCDM(H0=70, Om0=0.3)
+
+DL_DR7 = cosmo.luminosity_distance(z_DR7)
+DL_DR7 = DL_DR7.to(u.m)
+
+L_UV_DR7 = (10**(F_UV_DR7)/(1+z_DR7))*(4*np.pi*np.square(DL_DR7.value))
+
+# %% L_X DR7 
+
+FX_DR7 = 0.001*(FX_soft_DR7 + FX_hard_DR7) # ergs to J conversion factor
+
+LX_DR7 = (FX_DR7/(1+z_DR7))*(4*np.pi*np.square(DL_DR7.value))
+
+# %% DR12 UV
 
 # functions 
 def mag_to_flux(mags, mag_type):
@@ -247,7 +269,7 @@ def mag_to_flux(mags, mag_type):
     
         def S_WISE(mags,key):
             # Janksy flux * 10^-26 = Flux Wm^-2Hz^-1
-            return 10**(-26)*zero_points[str(key)]*10**(mags/-2.5)
+            return 10**(-26)*3631*zero_points[str(key)]*10**(mags/-2.5)
         
         # space for flux
         S_AB = np.zeros_like(mags)
@@ -319,8 +341,11 @@ def freq_flux_extract(freq_flux_list, key):
 # ugriz
 
 # ugriz
+
 ugriz_mags = t_DR12_DR12_matches['PSFMAG']
 ugriz_mags_err = t_DR12_DR12_matches['ERR_PSFMAG']
+ugriz_mags_err[np.abs(ugriz_mags_err) > 2] = np.nan
+ugriz_mags_err = np.ma.array(ugriz_mags_err, mask=np.isnan(ugriz_mags_err))
 
 # ugriz fluxes, effective freqs (mid band freqs)
 flux_DR12_ugriz, ugriz_freqs = mag_to_flux(ugriz_mags,'ugriz')
@@ -344,6 +369,10 @@ WISE_mags = np.array(WISE_mags).transpose()
 # mag errs
 WISE_mags_errs = [t_DR12_DR12_matches['ERR_W{}MAG'.format(i)] for i in range(1,5)]
 WISE_mags_errs = np.array(WISE_mags_errs).transpose()
+# remove errs with err < 100, then forward only the necessary WISE mags
+WISE_mags_errs[np.abs(WISE_mags_errs) > 10] = np.nan
+WISE_mags_errs = np.ma.array(WISE_mags_errs, mask=np.isnan(WISE_mags_errs))
+WISE_mags = WISE_mags*(WISE_mags_errs != True)
 
 # WISE fluxes and effective freqs
 flux_DR12_WISE, WISE_freqs = mag_to_flux(WISE_mags,'WISE')
@@ -353,7 +382,7 @@ WISE_Ang = np.array([34000, 46000, 120000, 220000])
 # WISE exinction for effective freqs
 WISE_ext = extinction.fm07(WISE_Ang, 1.0)
 # apply extinction to WISE fluxes
-flux_DR12_WISE = extinction.apply(WISE_ext,flux_DR12_WISE)
+flux_DR12_WISE = extinction.apply(WISE_ext, flux_DR12_WISE)
 # same but to errs
 flux_DR12_WISE_errs = extinction.apply(WISE_ext,flux_DR12_WISE_errs)
 
@@ -374,21 +403,10 @@ YJHK_Ang = np.array([10310, 12480, 16310, 22010])
 # YJHK extinction
 YJHK_ext = extinction.fm07(YJHK_Ang, 1.0)
 # YJHK flux
-flux_DR12_YJHK = np.zeros((len(flux_DR12_ugriz),4))
+flux_DR12_YJHK = np.zeros((len(flux_DR12_ugriz), 4))
 for i in range(0,4):
     for key in {'fY':fY,'fJ':fJ,'fH':fH,'fK':fK}.keys():
         flux_DR12_YJHK[:,i] = extinction.apply(YJHK_ext[i],{'fY':fY,'fJ':fJ,'fH':fH,'fK':fK}[key])
-
-
-
-
-
-
-
-
-
-
-# DR7 UV
 
 # matched freqs/fluxes
 #f_flux_YJHK = freqs_fluxes_match(YJHK_freqs, flux_DR12_YJHK)
@@ -446,8 +464,8 @@ fluxes_list = freq_flux_extract(f_flux_ugriz,'flux') + freq_flux_extract(f_flux_
 objects_ugriz, objects_WISE = object_all_bands(ff_arr_list)
 
 # object-wise arrays: f,flux,flux_err for ugriz/WISE 
-test_arr1 = np.concatenate((objects_ugriz, flux_DR12_ugriz_errs.reshape(4806,5,1)), axis=2)
-test_arr2 = np.concatenate((objects_WISE, flux_DR12_WISE_errs.reshape(4806,4,1)), axis=2)
+test_arr1 = np.concatenate((objects_ugriz, flux_DR12_ugriz_errs.reshape(len(flux_DR12_ugriz),5,1)), axis=2)
+test_arr2 = np.concatenate((objects_WISE, flux_DR12_WISE_errs.reshape(len(flux_DR12_WISE),4,1)), axis=2)
 
 # set values of (f,flux) with wrong errors to nan.
 test_arr1[(test_arr1[:,:,2] == np.inf) & (np.log10(test_arr1[:,:,1]) > -24)] = np.nan
@@ -458,29 +476,6 @@ all_arr = np.concatenate((test_arr1,test_arr2),axis=1)
 all_arr[all_arr[:,:,2] == np.inf] = np.nan
 all_arr[np.log10(all_arr[:,:,1]) > -24] = np.nan
 all_arr = np.ma.array(all_arr, mask=np.isnan(all_arr))
-
-# params = np.zeros(len()
-# for i in range(0,4806):
-#     plt.scatter(np.log10(all_arr[i][:,0]),np.log10(all_arr[i][:,1]))
-#     # plt.errorbar(np.log10(all_arr[i][:,0]),
-#     #             np.log10(all_arr[i][:,1]),
-#     #             np.log10(all_arr[i][:,2]),
-#     #             fmt='none',
-#     #             marker='o')
-#     if all_arr[i][:,2].any() != np.nan:
-        
-#         params += np.polyfit(
-#                 np.log10(all_arr[i][:,0]),
-#                 np.log10(all_arr[i][:,1]),
-#                 w=np.log10(all_arr[i][:,2]),
-#                 deg=1
-#                 )
-#     print(i)
-        
-# plt.grid(True)
-# plt.show()
-# params=params/4806
-# print(params[0]/4806,params[1]/4806)
 
 
 def F_UV_powerlaw(f_f_err_arr,lam):
@@ -500,26 +495,90 @@ def F_UV_powerlaw(f_f_err_arr,lam):
     freq_lam = c/(lam*1e-10)
     
     F_UV = np.zeros(len(all_arr),)
+    vF_v = np.zeros(len(all_arr),)
     params = np.zeros((len(f_f_err_arr),2))
     
     for i in range(0,len(f_f_err_arr)):
         
-        #optional plot 
-        #plt.scatter(np.log10(all_arr[i][:,0]),np.log10(all_arr[i][:,1]))
+        if  z_DR12[i] != -1.0:
+            #optional plot 
+            #plt.scatter(np.log10(all_arr[i][:,0]),np.log10(all_arr[i][:,1]))
+            
+            # fit each object across bands for blueshifted flux
+            params[i,:] = np.polyfit(
+                    x=np.log10(f_f_err_arr[i][:,0]),
+                    y=np.log10(f_f_err_arr[i][:,1]/z_DR12[i]),
+                    w=np.log10(f_f_err_arr[i][:,2]/z_DR12[i]),
+                    deg=1
+                    )
+            
+            F_UV[i] = np.log10(freq_lam)*params[i][0] + params[i][1]
+            
+        else:
+            pass
         
-        params[i,:] = np.polyfit(
-                x=np.log10(f_f_err_arr[i][:,0]),
-                y=np.log10(f_f_err_arr[i][:,1]),
-                w=np.log10(f_f_err_arr[i][:,2]),
-                deg=1
-                )
-    
-    
-        F_UV[i] = np.log10(freq_lam)*params[i][0] + params[i][1]
-    
     return F_UV    
 
+def nu_F_nu(f_f_err_arr):
+    # obtain vF_v values
+    
+    # take flux column and multiply each flux it by its own frequency
+    f_f_err_arr[:,:,1] = f_f_err_arr[:,:,0]*f_f_err_arr[:,:,1]
+    return f_f_err_arr
+    
+# blueshift flux, flux errors
+# z_DR12_arr = np.zeros((len(all_arr),9)) + 1
+# z_DR12_arr[:,0] = z_DR12#.reshape(len(z_DR12),)
+
+# z_DR12_arr = np.array(np.tile(z_DR12.transpose(),(9,1)))
+
+# all_arr[:,:,1], all_arr[:,:,2] = np.divide(all_arr[:,:,1],z_DR12_arr.transpose()), all_arr[:,:,1]/z_DR12_arr
+
+
+
+
+# function without logs for 1+z division?
+
+# log10 DR12 UV flux
 F_UV_DR12 = F_UV_powerlaw(all_arr, 2500)
+
+cosmo = FlatLambdaCDM(H0=70,Om0=0.3)
+
+DL_DR12 = cosmo.luminosity_distance(z_DR12)
+DL_DR12 = DL_DR12.to(u.m)
+
+#DL_DR12 = np.ma.array(D_L, mask=np.isnan(D_L))
+
+#D_L = 
+
+L_UV_DR12 = (F_UV_DR12/(1+z_DR12))*(4*np.pi*np.square(DL_DR12))
+
+# %% L_X DR12 
+
+FX_DR12 = 0.001*(FX_soft_DR12 + FX_hard_DR12) # 0.001 is erg/cm^2 to J/m^2
+
+# 0.001 erg/cm^2 -> J/m^2
+LX_DR12 = (FX_DR12/(1+z_DR12))*(4*np.pi*np.square(DL_DR12.value))
+
+
+plt.figure()
+plt.scatter(np.log10(all_arr[:,:,0]),np.log10(all_arr[:,:,0]*all_arr[:,:,1]))
+plt.xlabel('$\nu$')
+plt.ylabel('$\nuF_{\nu}$')
+plt.show()
+
+
+
+
+plt.figure()
+plt.plot(np.log10(L_UV_DR7),np.log10(LX_DR7),'x')
+plt.plot(np.log10(L_UV_DR12),np.log10(LX_DR12),'x')
+
+plt.show()
+
+
+
+
 
 
 
